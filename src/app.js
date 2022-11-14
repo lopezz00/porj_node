eeedcc2c5d123bfe6e8995c069e56477395cf83b
lopez00 <lopezz00>
2022-11-14 11:05:23 +0100

    Changes

--- src/app.js
+++ src/app.js
@@ -8,8 +8,9 @@ const app = express()
 
 app.use(express.json())
 
-app.use(indexRoutes)
-app.use('/api',userRoutes)
+app.use('/api', indexRoutes)
+app.use('/api', userRoutes)
+app.use('/api', maquinesRoutes)
 
 app.use((req, res, next) => {
     res.status(404).json({message: "Endpoint not found"})})

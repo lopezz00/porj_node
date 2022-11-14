aea7edeca5201e38a382fc7202994b6193de80d5
FrancescNosas <francescnosas@gmail.com>
2022-11-14 09:51:44 +0100

    Primer prototip funció add users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,3 +25,18 @@ export const getOneUser = async(req, res) => {
         return res.status(500).json({message: 'Someting went wrong'})
     }
 }
+// Creo un user un cop donat un json amb email, nom i cognoms
+export const createUser = async (req, res) => {
+    try {
+        const {email, nom, cognom1, cognom2} = req.body
+        const [rows] = await pool.query('INSERT INTO user(email, name, cognom1, cognom2) VALUES (?, ?, ?, ?)', [email, nom, cognom1, cognom2])
+    
+        // s'hauria de comprovar la id per evitar ids iguals?? com es faria el pw??
+
+        res.send({id: rows.insertId, email, nom, cognom1, cognom2,
+        })
+
+    } catch (e) {
+        return res.status(500).json({message: 'Someting went wrong'})
+    }
+}

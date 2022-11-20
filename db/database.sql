30afbcdfbb3d0976adcf3eeee1f9770e0585181f
lopez00 <lopezz00>
2022-11-20 14:05:22 +0100

    Petita explicació del scanvis que mybe haurem de fer a la bd

--- db/database.sql
+++ db/database.sql
@@ -1,3 +1,71 @@
+CREATE TABLE USUARI (
+    email VARCHAR(30) NOT NULL PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    cognom1 VARCHAR(30) NOT NULL,
+    cognom2 VARCHAR(30),
+    pw_hash VARCHAR(32) NOT NULL,               -- Contindrà el hash de la contrasenya de l'usuari. No salt. Sha(256) s'espera.
+    rol VARCHAR(30) NOT NULL,
+    id VARCHAR(8) NOT NULL,                     -- Valor targta RFID
+    PRIMARY key (email)
+);
+
+CREATE TABLE MAQUINA (
+    id_maquina INTEGER NOT NULL,
+    id_lab INTEGER,
+    nom_maquina VARCHAR(30) NOT NULL,
+    descripcio VARCHAR(120),
+    estat INTEGER NOT NULL,                     --2 apagada, 3 encesa, 4 fora de servei
+    email VARCHAR(30) NOT NULL,
+    calibracio REAL,
+    PRIMARY KEY (id_maquina),
+    FOREIGN KEY (id_lab) REFERENCES TECHLAB(id_techlab) ON DELETE CASCADE
+);
+
+CREATE TABLE TECHLAB (
+    id_techlab INTEGER,
+    aforament_max INTEGER NOT NULL,             -- Numero de maquines maximes que hi poiden haver al lab
+    aforament_actual INTEGER NOT NULL,          -- Numero de mauqines existents al lab actualment
+    maquines_numero INTEGER NOT NULL,           -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    maquines_ocupades INTEGER NOT NULL,         -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    PRIMARY key (id_techlab)
+);
+
+CREATE TABLE RESERVES (
+    email VARCHAR(30) NOT NULL,
+    id_maquina INTEGER NOT NULL,
+    hora_entrada DATE NOT NULL,
+    hora_sortida DATE NOT NULL,
+    data DATE NOT NULL,
+    reserva_feta INT NOT NULL,                  -- 0 RESERVA SENSE ENCARA USUARI, 1 RESERVA AMB USUARI, 2 HA ACABAT LA RESERVA
+    PRIMARY KEY (email, id_maquina, hora_entrada, data),
+    FOREIGN KEY (email) REFERENCES USUARI(email) ON DELETE CASCADE,
+    FOREIGN KEY (id_maquina) REFERENCES MAQUINA(id_maquina) ON DELETE CASCADE
+);
+
+CREATE TABLE HISTORIAL(
+    id INTEGER AUTO_INCREMENT,
+    id_maquina INTEGER NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    hora_entrada DATE NOT NULL,
+    hora_sortida DATE NOT NULL,
+    hora_consum DATE NOT NULL,                  -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi ja que volem el consum total i no per hores
+    data DATE NOT NULL,
+    consum INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_maquina) REFERENCES MAQUINA(id_maquina) ON DELETE CASCADE,
+    FOREIGN KEY (email) REFERENCES USUARI(email) ON DELETE CASCADE
+);
+
+CREATE TABLE CONFIGMAQUINES(                    -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    nom_maquina VARCHAR(30) NOT NULL,           -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    ssid_wifi VARCHAR(30) NOT NULL,             -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    pswd_wifi VARCHAR(30) NOT NULL,             -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+    PRIMARY KEY (nom_maquina)                   -- Diria que no ho farem servir, ja ho eliminarem de la bd en el cas de que sigui aixi
+);
+
+
+
+
 -- CREATE DATABASE IF NOT EXISTS companydb;
 
 -- USE companydb;
@@ -19,71 +87,5 @@
 
 -- SELECT * FROM employee;
 
-
-CREATE TABLE USUARI (
-    email VARCHAR(30) NOT NULL PRIMARY KEY,
-    nom VARCHAR(30) NOT NULL,
-    cognom1 VARCHAR(30) NOT NULL,
-    cognom2 VARCHAR(30),
-    pw_hash VARCHAR(32) NOT NULL,   -- Contindrà el hash de la contrasenya de l'usuari. No salt. Sha(256) s'espera.
-    rol VARCHAR(30) NOT NULL,
-    id VARCHAR(8) NOT NULL,         -- Valor targta RFID
-    PRIMARY key (email)
-);
-
-CREATE TABLE MAQUINA (
-    id_maquina INTEGER NOT NULL,
-    id_lab INTEGER,
-    nom_maquina VARCHAR(30) NOT NULL,
-    descripcio VARCHAR(120),
-    estat INTEGER NOT NULL, --2 apagada, 3 encesa, 4 fora de servei
-    email VARCHAR(30) NOT NULL,
-    calibracio REAL,
-    PRIMARY KEY (id_maquina),
-    FOREIGN KEY (id_lab) REFERENCES TECHLAB(id_techlab) ON DELETE CASCADE
-);
-
-CREATE TABLE TECHLAB (
-    id_techlab INTEGER,
-    aforament_max INTEGER NOT NULL,
-    aforament_actual INTEGER NOT NULL,
-    maquines_numero INTEGER NOT NULL,
-    maquines_ocupades INTEGER NOT NULL,
-    PRIMARY key (id_techlab)
-);
-
-CREATE TABLE RESERVES (
-    email VARCHAR(30) NOT NULL,
-    id_maquina INTEGER NOT NULL,
-    hora_entrada DATE NOT NULL,
-    hora_sortida DATE NOT NULL,
-    data DATE NOT NULL,
-    reserva_feta INT NOT NULL, --0 RESERVA SENSE ENCARA USUARI 1 RESERVA AMB USUARI, 2 HA ACABAT LA RESERVA
-    PRIMARY KEY (email, id_maquina, hora_entrada, data),
-    FOREIGN KEY (email) REFERENCES USUARI(email) ON DELETE CASCADE,
-    FOREIGN KEY (id_maquina) REFERENCES MAQUINA(id_maquina) ON DELETE CASCADE
-);
-
-CREATE TABLE HISTORIAL(
-    id INTEGER AUTO_INCREMENT,
-    id_maquina INTEGER NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    hora_entrada DATE NOT NULL,
-    hora_sortida DATE NOT NULL,
-    hora_consum DATE NOT NULL,
-    data DATE NOT NULL,
-    consum INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_maquina) REFERENCES MAQUINA(id_maquina) ON DELETE CASCADE,
-    FOREIGN KEY (email) REFERENCES USUARI(email) ON DELETE CASCADE
-);
-
-CREATE TABLE CONFIGMAQUINES(
-    nom_maquina VARCHAR(30) NOT NULL,
-    ssid_wifi VARCHAR(30) NOT NULL,
-    pswd_wifi VARCHAR(30) NOT NULL,
-    PRIMARY KEY (nom_maquina)
-);
-
-INSERT INTO USUARI (email,nom,cognom1,cognom2,pw_hash,rol,id) VALUES
-    ('upctech.solutions@gmail.com','Marc','Lopez','Arevalo','1234', 'Administrador',"c6888a9b");
+-- INSERT INTO USUARI (email,nom,cognom1,cognom2,pw_hash,rol,id) VALUES
+--     ('upctech.solutions@gmail.com','Marc','Lopez','Arevalo','1234', 'Administrador',"c6888a9b");
